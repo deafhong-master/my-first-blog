ec123295b636e8aa2535b17314c33081c86027c4
jwhong <hjw8613@gmail.com>
2017-10-11 12:00:33 +0900

    modify post_edit

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q*2$==eysxpm1nu5qcv0^^e-7jbd1e+fpklchh)p^fxkc2=mt!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
